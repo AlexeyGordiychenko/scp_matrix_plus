360e62e224e3161ad7a5287623a413fff5cf0480
elidacon <elidacon@student.21-school.ru>
2023-09-05 23:52:54 +0500

    Add constructor tests

---
+++ src/tests/s21_constructor_tests.cc
@@ -0,0 +1,44 @@
+
+#include "s21_matrix_oop_tests.h"
+
+TEST(Constructors, 1) {
+  S21Matrix m1;
+  EXPECT_EQ(m1.GetRows(), 1);
+  EXPECT_EQ(m1.GetCols(), 1);
+}
+
+TEST(Constructors, 2) {
+  S21Matrix m1(5, 1);
+  EXPECT_EQ(m1.GetRows(), 5);
+  EXPECT_EQ(m1.GetCols(), 1);
+}
+
+TEST(Constructors, 3) {
+  EXPECT_THROW(S21Matrix m1(-2, 1), std::invalid_argument);
+}
+
+TEST(Constructors, 4) {
+  EXPECT_THROW(S21Matrix m1(-2, -3), std::invalid_argument);
+}
+
+TEST(Constructors, 5) {
+  EXPECT_THROW(S21Matrix m1(0, 0), std::invalid_argument);
+}
+
+TEST(Constructors, 6) {
+  S21Matrix m1({2, 4});
+  EXPECT_EQ(m1.GetRows(), 2);
+  EXPECT_EQ(m1.GetCols(), 4);
+}
+
+TEST(Constructors, 7) {
+  S21Matrix m1({3, 2, 2.467, 8.14, 5.21, 8.23745, 1.1657, 0.5678});
+  EXPECT_EQ(m1.GetRows(), 3);
+  EXPECT_EQ(m1.GetCols(), 2);
+}
+
+TEST(Constructors, 8) {
+  EXPECT_THROW(S21Matrix m1({1}), std::invalid_argument);
+}
+
+TEST(Constructors, 9) { EXPECT_THROW(S21Matrix m1({}), std::invalid_argument); }
